37d18f8f2764218b5af3cfb9d36bb8c9efc40898
Zairo Bastos <zairo_vianahd@hotmail.com>
2022-07-13 14:24:16 -0300

    Criando rota de autenticação

---
+++ src/controllers/authController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import JWT from "jsonwebtoken";
+
+type user = {
+	id: string;
+	email: string;
+};
+
+export const Authorization = (req: Request, res: Response) => {
+	const { token } = req.body;
+	if (token) {
+		const decoded: user | any = JWT.verify(
+			token,
+			process.env.JWT_SECRET as string
+		);
+		if (decoded) {
+			res.json({ user: { id: decoded.id, email: decoded.email } });
+		} else {
+			res.json({ message: "Token inválido!" }).status(400);
+		}
+	}
+};
